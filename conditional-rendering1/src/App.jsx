044d30ce836009a3cecd9ed4588e59c047a49525
Vittorio De Giorgio <vdegiorgio8@gmail.com>
2024-04-25 20:49:31 +0200

    Aggiunto Esercizio: Components4

--- conditional-rendering1/src/App.jsx
+++ conditional-rendering1/src/App.jsx
@@ -1,4 +1,5 @@
 import Hello from './Hello'
+import Message from './assets/Message'
 
 function App() {
 
@@ -6,6 +7,15 @@ function App() {
     <>
       <div>
         <Hello></Hello>
+        <Hello></Hello>
+        <Hello></Hello>
+        { /* Risposta Components3: Si posso utilizzare il componente Hello più volte
+        e se lo faccio non faccio altro che renderizzare a schermo più volte la 
+        stessa cosa, ovvero il contenuto di Hello. */}
+        <Message></Message>
+        { /* Risposta Components3: Si posso utilizzare il componente Message direttamente
+        qui e se lo faccio funziona esattamente come nell'altro caso. */}
+
       </div>
     </>
   )

2d47bcf35d490eb7fe27a4ef84bbdf90aec9839f
Vittorio De Giorgio <vdegiorgio8@gmail.com>
2024-04-28 14:35:28 +0200

    Aggiunto Esercizio: HandlingEvents2

--- Event/src/MouseClicker.jsx
+++ Event/src/MouseClicker.jsx
@@ -1,10 +1,16 @@
 export default function MouseClicker() {
-
     function handleClick(e) {
-        console.log(e.target.name);
+        if (e.target.tagName === "IMG") {
+            console.log("Image src:", e.target.src);
+        } else {
+            console.log("Button name:", e.target.name);
+        }
     }
 
     return (
-        <button name="one" onClick={handleClick}>Mouse Clicker</button>
-    )
+        <button name="one" onClick={handleClick}>
+            Mouse Clicker
+            <img src="Event/public/vite.svg" alt="Immagine" />
+        </button>
+    );
 }

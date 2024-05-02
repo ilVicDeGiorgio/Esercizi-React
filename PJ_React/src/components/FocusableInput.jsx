9bd35ce8e20ae82bdd65781b5111991788db94c3
Vittorio De Giorgio <vdegiorgio8@gmail.com>
2024-05-02 22:59:06 +0200

    Modificato l'esercizio: Refs1

--- PJ_React/src/components/FocusableInput.jsx
+++ PJ_React/src/components/FocusableInput.jsx
@@ -1,6 +1,17 @@
+import { useEffect } from 'react'
+import { useRef } from 'react'
+
 function FocusableInput() {
+    const inputRef = useRef(3)
+
+    useEffect(() => {
+        if (inputRef.current) {
+            inputRef.current.focus()
+        }
+    }, [])
+
     return (
-        <input type="text" autoFocus />
+        <input type="text" ref={inputRef} />
     )
 }
 

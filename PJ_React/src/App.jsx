e3baa76623b8abd14f1c519948cf5eb32cebfa9a
Vittorio De Giorgio <vdegiorgio8@gmail.com>
2024-05-13 00:32:41 +0200

    Aggiunto Esercizio: Context

--- PJ_React/src/App.jsx
+++ PJ_React/src/App.jsx
@@ -1,11 +1,19 @@
 import Container from "./components/Container";
+import Clock from "./components/Clock";
+import LanguageContext from "./LenguageContext";
 import { useState } from "react";
 
 function App() {
   const [collapse, setCollapse] = useState(false);
+  const [language, setSelectLanguage] = useState("en");
 
   const handleCollapse = () => {
     setCollapse(!collapse);
+  };
+
+  const handleSelectLanguage = (e) => {
+    setSelectLanguage(e.target.value);
+    console.log(language);
   };
   return (
     <Container
@@ -16,26 +24,33 @@ function App() {
         </div>
       }
     >
-      {collapse && (
-        <div>
-          <p>Corpo del container</p>
-          <img
-            src="https://loremipsum.io/assets/images/lorem-ipsum-generator-custom-placeholder-text.jpg"
-            alt=""
-            height="300px"
-          />
-          <br />
-          <cite>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae nemo
-            earum nam ipsa? Aut molestias aliquam numquam debitis magni fugit
-            voluptatibus maxime esse? Tenetur, similique nobis. Exercitationem
-            debitis consequatur ab! Lorem ipsum, dolor sit amet consectetur
-            adipisicing elit. Harum vel voluptatum atque voluptate omnis ex
-            obcaecati quisquam nihil sunt. Totam eaque temporibus at, tenetur
-            magni magnam nemo labore eius praesentium.
-          </cite>
-        </div>
-      )}
+      <LanguageContext.Provider value={language}>
+        <select value={language} onChange={handleSelectLanguage} name="" id="">
+          <option value="it">IT</option>
+          <option value="en">EN</option>
+        </select>
+        <Clock></Clock>
+        {collapse && (
+          <div>
+            <p>Corpo del container</p>
+            <img
+              src="https://loremipsum.io/assets/images/lorem-ipsum-generator-custom-placeholder-text.jpg"
+              alt=""
+              height="300px"
+            />
+            <br />
+            <cite>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quae
+              nemo earum nam ipsa? Aut molestias aliquam numquam debitis magni
+              fugit voluptatibus maxime esse? Tenetur, similique nobis.
+              Exercitationem debitis consequatur ab! Lorem ipsum, dolor sit amet
+              consectetur adipisicing elit. Harum vel voluptatum atque voluptate
+              omnis ex obcaecati quisquam nihil sunt. Totam eaque temporibus at,
+              tenetur magni magnam nemo labore eius praesentium.
+            </cite>
+          </div>
+        )}
+      </LanguageContext.Provider>
     </Container>
   );
 }
---
+++ PJ_React/src/LenguageContext.jsx
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+
+const LenguageContext = createContext("en");
+
+export default LenguageContext;

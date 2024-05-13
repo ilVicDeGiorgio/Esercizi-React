9bc6663c999f9d1c8f48a7cbcdc0b2d068153c8c
Vittorio De Giorgio <vdegiorgio8@gmail.com>
2024-05-13 19:50:23 +0200

    Aggiunte prove del Router Dom

--- PJ_React/src/App.jsx
+++ PJ_React/src/App.jsx
@@ -1,11 +1,15 @@
-import Container from "./components/Container";
 import GHT2 from "./components/GHT2";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Container>
-      <GHT2></GHT2>
-    </Container>
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>TITOLO HOMEPAGE</h1>} />
+        <Route path="/github" element={<GHT2></GHT2>} />
+        <Route path="/button" element={<button>BOTTONE MERAVIGLIOSO</button>} />
+      </Routes>
+    </Router>
   );
 }
 

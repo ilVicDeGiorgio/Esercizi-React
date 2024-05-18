e1f86468cd89d9370eea3589f067167cdbfe2a82
Vittorio De Giorgio <vdegiorgio8@gmail.com>
2024-05-18 16:55:41 +0200

    Aggiunto Esercizio: ReactRouter2

--- PJ_React/src/App.jsx
+++ PJ_React/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Welcome from "./components/Welcome";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Welcome name="Vittorio" />} />
+        <Route path="/Counter" element={<Counter />} />
       </Routes>
     </Router>
   );

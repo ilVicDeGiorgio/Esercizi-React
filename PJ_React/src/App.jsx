715e8fccc7f8cce57b2bac8dc57e48cbacf076c7
Vittorio De Giorgio <vdegiorgio8@gmail.com>
2024-05-18 17:33:37 +0200

    Aggiunto Esercizio: ReactRouter4 nel branch precedente

--- PJ_React/src/App.jsx
+++ PJ_React/src/App.jsx
@@ -1,16 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Welcome from "./components/Welcome";
 import ShowGithubUser from "./components/ShowGithubUser";
+import Counter from "./components/Counter";
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Welcome name="Vittorio" />} />
+        <Route path="/welcome" element={<Welcome name="Vittorio" />} />
         <Route
           path="/user"
           element={<ShowGithubUser username={"ilVicDeGiorgio"} />}
         />
+        <Route path="/Counter" element={<Counter />} />
       </Routes>
+
+      <Link to="/counter">
+        <p>Counter</p>
+      </Link>
+      <Link to="/user">
+        <p>ShowGithubUser</p>
+      </Link>
+      <Link to="/welcome">
+        <p>Welcome!</p>
+      </Link>
     </Router>
   );
 }
